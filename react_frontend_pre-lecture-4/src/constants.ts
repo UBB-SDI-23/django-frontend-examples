ba117972a3a2b1eeea38a13d36bcb919c59e36ba
Vlad-Sebastian Ionescu <ionescu.vlad1@gmail.com>
2023-03-23 13:25:53 +0200

    Prod vs env consts

--- react_frontend_pre-lecture-4/src/constants.ts
+++ react_frontend_pre-lecture-4/src/constants.ts
@@ -1,1 +1,5 @@
-export const BACKEND_API_URL = "/api";
+const PROD_BACKEND_API_URL = "/api";
+const DEV_BACKEND_API_URL = "http://127.0.0.1:8000/api";
+
+export const BACKEND_API_URL =
+	process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL;
